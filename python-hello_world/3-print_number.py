6441db3f87dc77c4829dfd5feb19cc2c3e06dd81
Mordecai Nayituriki <m.nayiturik@alustudent.com>
2025-02-22 06:46:17 -0800

    print the integer stored in the variable number, followed by Battery street

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,5 @@
-#!/usr/bin/python3
-number = 123
-print(f"{number} Battery street")
+number = 333
+try:
+    print(f"{number} Battery street")
+except ValueError:
+    print("ValueError: Unknown format code 'd' for object of type 'str'")
