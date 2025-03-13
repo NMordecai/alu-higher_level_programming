db8854d8b9823f0e336244cb75b90b9325156640
Momo <m.nayiturik@alustudent.com>
2025-03-13 22:07:58 +0000

    still updating

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-""" Load, add, save  """
-from sys import argv
+import sys
+import json
 
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file, using a JSON representation."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+def load_from_json_file(filename):
+    """Creates an Object from a JSON file."""
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
 
-""" Script that adds all arguments to a Python list,
-and then save them to a file """
-try:
-    add_i = load_from_json_file('add_item.json')
-    save_to_json_file(add_i + argv[1:], 'add_item.json')
-except Exception:
-    save_to_json_file(argv[1:], 'add_item.json')
+filename = "add_item.json"
+my_list = load_from_json_file(filename)
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+
+save_to_json_file(my_list, filename)
