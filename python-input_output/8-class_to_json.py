1ebbcf4afba9616c2e454641eef2b1cf066b586c
Mordecai Nayituriki <m.nayiturik@alustudent.com>
2025-03-13 07:17:55 -0700

    a function that returns the dictionary description with simple data structure

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a Class.
+
+    Returns:
+        dict: A dictionary representation of the object's attributes.
+    """
+    if not hasattr(obj, "__dict__"):
+        return {}
+    
+    json_dict = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+    return json_dict
